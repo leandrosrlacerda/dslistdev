c01a6a8632e1724a3f515582ffdcc79dbf190f09
leandro <leandrolacerda.sistema@gmail.com>
2025-08-19 10:18:43 -0300

    Project created

---
+++ src/main/java/com/devsuperior/dslistdev/DslistdevApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslistdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistdevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/dslistdev/DslistdevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslistdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
